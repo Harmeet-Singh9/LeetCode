4aa4f46719cd721a7707eb15d2a97cc81f6bc532
Harmeet Singh Dhingra <harmeet.dhingra9@gmail.com>
2020-06-13 13:41:29 -0700

    Add project files.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Leetcode_1_Two_Sum
+    {
+        public Leetcode_1_Two_Sum()
+        {
+
+        }
+        public int[] TwoSum(int[] nums, int target)
+        {
+            if (nums == null || nums.Length < 2)
+                return new int[0];
+
+            var visited = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var current = nums[i];
+                var search = target - current;
+
+                if (visited.ContainsKey(search))
+                {
+                    return new int[] { visited[search], i };
+                }
+                else
+                {
+                    if (!visited.ContainsKey(current))
+                    {
+                        visited.Add(current, i);
+                    }
+                }
+            }
+
+            return new int[0];
+        }
+    }
+}
